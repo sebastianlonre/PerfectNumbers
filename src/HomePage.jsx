5958a1240e229694d5dfd774325ac586d6cebad7
sebas <sebastian.londono75@correo.tdea.edu.co>
2024-04-15 23:46:45 -0500

    add comments

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,9 +3,11 @@ import { PerfectNumber } from './components/PerfectNumber';
 
 export const HomePage = () => {
 
+  //se utiliza local storage para almacenar los datos que ingreso el usuario por ultima vez
   const [minRange, setMinRange] = useState(() => {
 
     const savedMinRange = localStorage.getItem('minRange');
+    //Si no encuentra un valor de rango maximo por defecto coloca 6
     return savedMinRange ? parseInt(savedMinRange, 10) : 6;
 
   });
@@ -13,10 +15,11 @@ export const HomePage = () => {
   const [maxRange, setMaxRange] = useState(() => {
 
     const savedMaxRange = localStorage.getItem('maxRange');
+    //Si no encuentra un valor de rango maximo por defecto coloca 500
     return savedMaxRange ? parseInt(savedMaxRange, 10) : 500;
-
   });
 
+  //Actualización de los estados de los input
   const handleMinRangeChange = (e) => {
     const newValue = parseInt(e.target.value);
     setMinRange(newValue);
--- src/components/PerfectNumber.jsx
+++ src/components/PerfectNumber.jsx
@@ -4,14 +4,21 @@ export const PerfectNumber = ({ minRange, maxRange }) => {
 
     const [perfectNumbers, setPerfectNumbers] = useState([]);
 
+    //Algortimo para determinar si un numero es perfecto
     const IsPerfectNumber =( number )=>{
     
         let sum = 1;
-
+        //En el ciclo se itera hasta la raíz cuadrada del número
+        //Porque en un numero perfecto su raiz siempre sera inferior
         for (let i = 2; i <= Math.sqrt(number); i++) {
-
+            //Se verifica que el valor de la i sea divisor de number
             if (number % i === 0) {
+
                 sum += i;
+
+                // Si i no es la raíz cuadrada de number, agrega
+                // el divisor correspondiente de i a la suma.
+
                 if (i !== Math.sqrt(number)) {
                     sum += number / i;
                 }
@@ -22,14 +29,17 @@ export const PerfectNumber = ({ minRange, maxRange }) => {
     }
 
     useEffect(() => {
-
+      // Función para encontrar números perfectos dentro del rango dado.
       const findPerfectNumbers = (minRange, maxRange) => {
+          // Ajusta el valor mínimo del rango si es menor que 2.
           if (minRange < 2) minRange = 2;
+          // Ajusta el valor mínimo del rango si está entre 6 y 28.
           if (minRange > 6 && minRange < 28) minRange = 28;
+          // Asegura que el valor mínimo del rango sea par.
           if (minRange % 2 !== 0) minRange++;
 
           let arrayPerfectNumbers = [];
-
+          //Itera a través del rango de numero solo en valores pares
           for (let i = minRange; i <= maxRange; i += 2) {
               if (IsPerfectNumber(i)) {
                   arrayPerfectNumbers.push(i);
